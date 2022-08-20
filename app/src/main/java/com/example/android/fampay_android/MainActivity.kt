0516886d0aea39b573cfde228e38abe2561a589d
Chirayu Sharma <thechirayu02@gmail.com>
2022-08-20 19:37:09 +0530

    add: building ui from api response and handling clicks

--- app/src/main/java/com/example/android/fampay_android/MainActivity.kt
+++ app/src/main/java/com/example/android/fampay_android/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.android.fampay_android
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -14,6 +21,7 @@ import androidx.compose.material.icons.outlined.Forward
 import androidx.compose.material.icons.outlined.NavigateNext
 import androidx.compose.material.icons.outlined.SkipNext
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,12 +32,35 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.android.fampay_android.ViewModels.MainViewModel
+import com.example.android.fampay_android.networking.data.CTA
+import com.example.android.fampay_android.networking.data.CardGroup
+import com.example.android.fampay_android.networking.data.Cards
+import com.example.android.fampay_android.networking.data.Response
 import com.example.android.fampay_android.ui.theme.FampayAndroidTheme
 
 class MainActivity : ComponentActivity() {
+    val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { FampayApp() }
+        setContent {
+            FampayAndroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ResponseItem(response = mainViewModel.response)
+                    mainViewModel.getResponse()
+                }
+            }
+//            HC6(
+//                image = "https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/128x128/plain/shape_square.png",
+//                title = "Setup your UPI ID",
+//                CTA = { /*TODO*/ }
+//            )
+//            FampayApp()
+        }
     }
 }
 
@@ -196,6 +227,88 @@ fun HC1(
 }
 
 @Composable
+fun ResponseItem(response: Response) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(15.dp)
+    ) {
+        itemsIndexed(items = response.card_groups) { index, item ->
+            CardGroupItem(cardGroup = item)
+        }
+    }
+}
+
+@Composable
+fun CardGroupItem(cardGroup: CardGroup) {
+    LazyRow {
+        itemsIndexed(items = cardGroup.cards) { index, item ->
+            CardItem(
+                cards = item,
+                design_type = cardGroup.design_type
+            )
+        }
+    }
+}
+
+@Composable
+fun CardItem(cards: Cards, design_type: String) {
+
+    val context = LocalContext.current
+    //val intent = remember { Intent(Intent.ACTION_VIEW, Uri.parse("")) }
+    when (design_type) {
+        "HC6" -> HC6(
+            image = cards.icon.image_url,
+            title = cards.description,
+            CTA = {
+                val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(cards.url))
+                context.startActivity(intent)
+            }
+        )
+
+        "HC9" -> HC9(
+            image = cards.bg_image.image_url,
+            aspectRatio = cards.bg_image.aspect_ratio,
+            CTA = {
+                val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(cards.url))
+                context.startActivity(intent)
+            }
+        )
+
+        "HC3" -> HC3(
+            image = cards.bg_image.image_url,
+            title = cards.title,
+            description = cards.description,
+            aspectRatio = cards.bg_image.aspect_ratio,
+            cardCTA = {
+                val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(cards.url))
+                context.startActivity(intent)
+            },
+            buttonCTA = {
+                val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(cards.cta[0].url))
+                context.startActivity(intent)
+            }
+        )
+
+        "HC5" -> HC5(
+            image = cards.bg_image.image_url,
+            CTA = {
+                val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(cards.url))
+                context.startActivity(intent)
+            }
+        )
+
+        "HC1" -> HC1(
+            image = cards.icon.image_url,
+            title = cards.title,
+            bgColor = Color.White,
+            CTA = {
+                val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(cards.url))
+                context.startActivity(intent)
+            }
+        )
+        else -> Text(text = "None")
+    }
+}
+
+@Composable
 fun FampayApp() {
 //    val context = LocalContext.current
 //    HC3(
@@ -222,12 +335,12 @@ fun FampayApp() {
 //        aspectRatio = 0.84F,
 //        CTA = {}
 //    )
-    HC1(
-        image = "https://westeros-staging.s3.amazonaws.com/media/images/generic/4ce76db9e755497f8d176764b6d590ba.png",
-        title = "Small display card",
-        bgColor = Color.White,
-        CTA = {}
-    )
+//    HC1(
+//        image = "https://westeros-staging.s3.amazonaws.com/media/images/generic/4ce76db9e755497f8d176764b6d590ba.png",
+//        title = "Small display card",
+//        bgColor = Color.White,
+//        CTA = {}
+//    )
 }
 
 

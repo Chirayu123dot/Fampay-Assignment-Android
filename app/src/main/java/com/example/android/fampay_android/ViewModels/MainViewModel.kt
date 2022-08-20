36105818d0a9f18441a1984821dbb135520512e5
Chirayu Sharma <thechirayu02@gmail.com>
2022-08-20 18:16:51 +0530

    add: ViewModel implementation

--- app/src/main/java/com/example/android/fampay_android/ViewModels/MainViewModel.kt
+++ app/src/main/java/com/example/android/fampay_android/ViewModels/MainViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.android.fampay_android.ViewModels
 
-class MainViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.fampay_android.networking.ApiService
+import com.example.android.fampay_android.networking.data.Response
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    var response: Response by mutableStateOf(Response())
+    var errorMessage: String by mutableStateOf("")
+
+    fun getResponse() {
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance()
+            try {
+                val apiResponse = apiService.getResponse()
+                response = apiResponse
+            } catch (e: Exception) {
+                errorMessage = e.message.toString()
+            }
+        }
+    }
 }
